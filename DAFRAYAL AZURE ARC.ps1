1701c482710b5145497075d71ad0c74b8b18879f
Niklavs Beikerts <dafrayal@avigedsfcmxrozb.onmicrosoft.com>
2024-03-19 02:39:43 +0200

    fwl

---
+++ DAFRAYAL AZURE ARC.ps1
@@ -0,0 +1,27 @@
+try {
+    $env:SUBSCRIPTION_ID = "2a12bbb3-367b-4826-a20f-5df435cb7647";
+    $env:RESOURCE_GROUP = "exzo";
+    $env:TENANT_ID = "f063d70e-504b-428d-a12d-cc5d18f5fb2f";
+    $env:LOCATION = "koreacentral";
+    $env:AUTH_TYPE = "token";
+    $env:CORRELATION_ID = "b292c965-bb1b-404f-a8d5-0fa1fa3b2aa3";
+    $env:CLOUD = "AzureCloud";
+    
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor 3072;
+
+    # Download the installation package
+    Invoke-WebRequest -UseBasicParsing -Uri "https://aka.ms/azcmagent-windows" -TimeoutSec 30 -OutFile "$env:TEMP\install_windows_azcmagent.ps1";
+
+    # Install the hybrid agent
+    & "$env:TEMP\install_windows_azcmagent.ps1";
+    if ($LASTEXITCODE -ne 0) { exit 1; }
+
+    # Run connect command
+    & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect --resource-group "$env:RESOURCE_GROUP" --tenant-id "$env:TENANT_ID" --location "$env:LOCATION" --subscription-id "$env:SUBSCRIPTION_ID" --cloud "$env:CLOUD" --tags "Host=1" --correlation-id "$env:CORRELATION_ID";
+}
+catch {
+    $logBody = @{subscriptionId="$env:SUBSCRIPTION_ID";resourceGroup="$env:RESOURCE_GROUP";tenantId="$env:TENANT_ID";location="$env:LOCATION";correlationId="$env:CORRELATION_ID";authType="$env:AUTH_TYPE";operation="onboarding";messageType=$_.FullyQualifiedErrorId;message="$_";};
+    Invoke-WebRequest -UseBasicParsing -Uri "https://gbl.his.arc.azure.com/log" -Method "PUT" -Body ($logBody | ConvertTo-Json) | out-null;
+    Write-Host  -ForegroundColor red $_.Exception;
+}
